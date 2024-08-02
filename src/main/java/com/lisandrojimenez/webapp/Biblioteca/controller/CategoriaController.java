e5cc582b07657a153da3c8f268cca97a9d82ecdf
Informatica <informatica@alumno.local>
2024-08-02 16:19:15 -0600

    CRUD Agregados

--- src/main/java/com/lisandrojimenez/webapp/Biblioteca/controller/CategoriaController.java
+++ src/main/java/com/lisandrojimenez/webapp/Biblioteca/controller/CategoriaController.java
@@ -6,14 +6,19 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lisandrojimenez.webapp.Biblioteca.model.Categoria;
 import com.lisandrojimenez.webapp.Biblioteca.service.CategoriaService;
+
+import io.micrometer.core.ipc.http.HttpSender.Response;
 
 @Controller
 @RestController
@@ -22,11 +27,23 @@ public class CategoriaController {
     @Autowired
     CategoriaService categoriaService;
 
+    //listar
     @GetMapping("/")
     public List<Categoria> listaCategorias() {
         return categoriaService.listarCategorias();
     }
 
+    //Buscar
+    @GetMapping("/{id}")
+    public ResponseEntity<Categoria> buscarCategoriaPorId(@PathVariable Long id){
+        try {
+            return ResponseEntity.ok(categoriaService.buscarCategoriaPorId(id));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(null);
+        }
+    }
+
+    //agregar
     @PostMapping("/")
     public ResponseEntity<Map<String, Boolean>> agregarCategoria(@RequestBody Categoria categoria){
         Map<String, Boolean> response = new HashMap<>();
@@ -36,8 +53,42 @@ public class CategoriaController {
             response.put("se agrego con exito", true);
             return ResponseEntity.ok(response);
         }catch(Exception e){
-            response.put("se agrego con exito.", false);
+            response.put("no se pudo agregar", false);
             return ResponseEntity.badRequest().body(response);
         }
     }
+
+    //Editar
+    @PutMapping("/{id}")
+    public ResponseEntity<Map<String, String>> editarCategoria(@PathVariable Long id, @RequestBody Categoria categoriaNueva){
+        Map<String, String> response = new HashMap<>();
+        try {
+            Categoria categoria = categoriaService.buscarCategoriaPorId(id);
+            categoria.setNombreCategoria(categoriaNueva.getNombreCategoria());
+            categoriaService.guardarCategoria(categoria);
+            response.put("message","Categoria se ha editado con exito");
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            response.put("message", "No se pudo editar la categoria");
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+    
+    //Eliminar
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Map<String, String>>eliminarCategoria(@PathVariable Long id){
+        Map<String, String> response = new HashMap<>();
+        try {
+            Categoria categoria = categoriaService.buscarCategoriaPorId(id);
+            categoriaService.eliminarCategoria(categoria);
+            response.put("message", "Eliminado con exito");
+            return ResponseEntity.ok(response);
+        } catch (Exception e) {
+            response.put("message", "La categoria no se pudo eliminar");
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+    
 }
